db1c398fe09fba670142dd3a201d23cc04435cb0
mar_b <mar_b@epitech.eu>
2015-07-28 22:36:57 +0200

    .h file removed -> useless

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include "palindrome.h"
 
 /**
  * @file main.c
--- palindrome.h
+++
@@ -1,7 +0,0 @@
-#ifndef PALINDROME_H_
-# define PALINDROME_H_
-
-# define SZ_OCTET	8
-# define NB_BITS	sizeof(unsigned int) * SZ_OCTET
-
-#endif /* !PALINDROME_H_ */
